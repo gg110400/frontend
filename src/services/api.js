5fa94220b5dbedae91256422f898e90a4119eedb
gg110402 <giuliagiudici110402@gmail.com>
2024-07-23 10:39:31 +0200

    committ

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL='https://backend-del-blog.onrender.com/api' || 'http://localhost:3000';
+const API_URL='https://backend-blog-80o6.onrender.com/api' || 'http://localhost:3000/api';
 const api=axios.create({baseURL:API_URL}); 
 
 api.interceptors.request.use(
